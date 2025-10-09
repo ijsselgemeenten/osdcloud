dbdd6859c916cb71961e9854c9198c40467c1871
semidoludizgin <137872182+semidoludizgin@users.noreply.github.com>
2025-10-09 10:55:14 +0200

    Update winget.osdcloud.com.ps1

--- winget.osdcloud.com.ps1
+++ winget.osdcloud.com.ps1
@@ -1,39 +1,15 @@
-$hasPackageManager = Get-AppPackage -name 'Microsoft.DesktopAppInstaller'
-if (!$hasPackageManager -or [version]$hasPackageManager.Version -lt [version]"1.10.0.0") {
-    "Installing winget Dependencies"
-    Add-AppxPackage -Path 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
-    $releases_url = 'https://api.github.com/repos/microsoft/winget-cli/releases/latest'
+# get latest download url
+$URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
+$URL = (Invoke-WebRequest -Uri $URL).Content | ConvertFrom-Json |
+        Select-Object -ExpandProperty "assets" |
+        Where-Object "browser_download_url" -Match '.msixbundle' |
+        Select-Object -ExpandProperty "browser_download_url"
 
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    $releases = Invoke-RestMethod -uri $releases_url
-    $latestRelease = $releases.assets | Where { $_.browser_download_url.EndsWith('msixbundle') } | Select -First 1
+# download
+Invoke-WebRequest -Uri $URL -OutFile "Setup.msix" -UseBasicParsing
 
-    "Installing winget from $($latestRelease.browser_download_url)"
-    Add-AppxPackage -Path $latestRelease.browser_download_url
-}
-else {
-    "winget already installed"
-}
-#### Creating settings.json #####
+# install
+Add-AppxPackage -Path "Setup.msix"
 
-if ([System.Security.Principal.WindowsIdentity]::GetCurrent().IsSystem) {
-        $SettingsPath = "$Env:windir\system32\config\systemprofile\AppData\Local\Microsoft\WinGet\Settings\settings.json"
-    }else{
-        $SettingsPath = "$env:LOCALAPPDATA\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json"
-    }
-    if (Test-Path $SettingsPath){
-        $ConfigFile = Get-Content -Path $SettingsPath | Where-Object {$_ -notmatch '//'} | ConvertFrom-Json
-    }
-    if (!$ConfigFile){
-        $ConfigFile = @{}
-    }
-    if ($ConfigFile.installBehavior.preferences.scope){
-        $ConfigFile.installBehavior.preferences.scope = "Machine"
-    }else {
-        Add-Member -InputObject $ConfigFile -MemberType NoteProperty -Name 'installBehavior' -Value $(
-            New-Object PSObject -Property $(@{preferences = $(
-                    New-Object PSObject -Property $(@{scope = "Machine"}))
-            })
-        ) -Force
-    }
-    $ConfigFile | ConvertTo-Json | Out-File $SettingsPath -Encoding utf8 -Force
+# delete file
+Remove-Item "Setup.msix"
