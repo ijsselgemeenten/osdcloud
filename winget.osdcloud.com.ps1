ea8f8a8b597982148942281949a4d0d5af96f4f4
semidoludizgin <137872182+semidoludizgin@users.noreply.github.com>
2025-10-09 09:47:57 +0200

    Update winget.osdcloud.com.ps1

--- winget.osdcloud.com.ps1
+++ winget.osdcloud.com.ps1
@@ -1,130 +1,38 @@
-<#PSScriptInfo
-.VERSION 23.6.10.1
-.GUID 8aa84227-ddb5-4276-95fb-ffb2d6121bf8
-.AUTHOR David Segura @SeguraOSD
-.COMPANYNAME osdcloud.com
-.COPYRIGHT (c) 2023 David Segura osdcloud.com. All rights reserved.
-.TAGS OSDeploy OSDCloud WinGet PowerShell
-.LICENSEURI 
-.PROJECTURI https://github.com/OSDeploy/OSD
-.ICONURI 
-.EXTERNALMODULEDEPENDENCIES 
-.REQUIREDSCRIPTS 
-.EXTERNALSCRIPTDEPENDENCIES 
-.RELEASENOTES
-Script should be executed in a Command Prompt using the following command
-powershell Invoke-Expression -Command (Invoke-RestMethod -Uri winget.osdcloud.com)
-This is abbreviated as
-powershell iex (irm winget.osdcloud.com)
-#>
-#Requires -RunAsAdministrator
-<#
-.SYNOPSIS
-    PowerShell Script which supports WinGet
-.DESCRIPTION
-    PowerShell Script which supports WinGet
-.NOTES
-    Version 23.9.28.1
-.LINK
-    https://raw.githubusercontent.com/OSDeploy/OSD/master/cloud/subdomains/winget.osdcloud.com.ps1
-.EXAMPLE
-    powershell iex (irm winget.osdcloud.com)
-#>
-[CmdletBinding()]
-param()
-$ScriptName = 'winget.osdcloud.com'
-$ScriptVersion = '23.9.28.1'
+#WebClient
+$dc = New-Object net.webclient
+$dc.UseDefaultCredentials = $true
+$dc.Headers.Add("user-agent", "Inter Explorer")
+$dc.Headers.Add("X-FORMS_BASED_AUTH_ACCEPTED", "f")
 
-#region Initialize
-$Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-$ScriptName.log"
-$null = Start-Transcript -Path (Join-Path "$env:SystemRoot\Temp" $Transcript) -ErrorAction Ignore
-
-if ($env:SystemDrive -eq 'X:') {
-    $WindowsPhase = 'WinPE'
+#temp folder
+$InstallerFolder = $(Join-Path $env:ProgramData CustomScripts)
+if (!(Test-Path $InstallerFolder))
+{
+New-Item -Path $InstallerFolder -ItemType Directory -Force -Confirm:$false
 }
-else {
-    $ImageState = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Setup\State' -ErrorAction Ignore).ImageState
-    if ($env:UserName -eq 'defaultuser0') {$WindowsPhase = 'OOBE'}
-    elseif ($ImageState -eq 'IMAGE_STATE_SPECIALIZE_RESEAL_TO_OOBE') {$WindowsPhase = 'Specialize'}
-    elseif ($ImageState -eq 'IMAGE_STATE_SPECIALIZE_RESEAL_TO_AUDIT') {$WindowsPhase = 'AuditMode'}
-    else {$WindowsPhase = 'Windows'}
-}
-
-Write-Host -ForegroundColor Green "[+] $ScriptName $ScriptVersion ($WindowsPhase Phase)"
-Invoke-Expression -Command (Invoke-RestMethod -Uri functions.osdcloud.com)
-#endregion
-
-#region Admin Elevation
-$whoiam = [system.security.principal.windowsidentity]::getcurrent().name
-$isElevated = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
-if ($isElevated) {
-    Write-Host -ForegroundColor Green "[+] Running as $whoiam (Admin Elevated)"
-}
-else {
-    Write-Host -ForegroundColor Red "[!] Running as $whoiam (NOT Admin Elevated)"
-    Break
-}
-#endregion
-
-#region Transport Layer Security (TLS) 1.2
-Write-Host -ForegroundColor Green "[+] Transport Layer Security (TLS) 1.2"
-[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
-#endregion
-
-#region WinPE
-if ($WindowsPhase -eq 'WinPE') {
-    Write-Host -ForegroundColor Red "[!] WinGet is not supported in WinPE"
-    $null = Stop-Transcript -ErrorAction Ignore
-}
-#endregion
-
-#region Specialize
-if ($WindowsPhase -eq 'Specialize') {
-    $null = Stop-Transcript -ErrorAction Ignore
-}
-#endregion
-
-#region AuditMode
-if ($WindowsPhase -eq 'AuditMode') {
-    $null = Stop-Transcript -ErrorAction Ignore
-}
-#endregion
-
-#region OOBE
-if ($WindowsPhase -eq 'OOBE') {
-    osdcloud-SetExecutionPolicy
-    osdcloud-SetPowerShellProfile
-    osdcloud-InstallPackageManagement
-    osdcloud-TrustPSGallery
-    osdcloud-InstallPowerShellModule -Name Pester
-    osdcloud-InstallPowerShellModule -Name PSReadLine
-    osdcloud-InstallWinGet
-    if (Get-Command 'WinGet' -ErrorAction SilentlyContinue) {
-        Write-Host -ForegroundColor Green "[+] winget upgrade --all --accept-source-agreements --accept-package-agreements"
-        winget upgrade --all --accept-source-agreements --accept-package-agreements
-    }
-    osdcloud-InstallPwsh
-    Write-Host -ForegroundColor Green "[+] winget.osdcloud.com Complete"
-    winget install --id Omnissa.HorizonClient -e --scope machine --silent --disable-interactivity --accept-package-agreements --accept-source-agreements --override "VDM_SERVER=vdi.ijsselgemeenten.nl DESKTOP_SHORTCUT=1 /norestart /silent"
-    $null = Stop-Transcript -ErrorAction Ignore
-}
-#endregion
-
-#region Windows
-if ($WindowsPhase -eq 'Windows') {
-    osdcloud-SetExecutionPolicy
-    osdcloud-SetPowerShellProfile
-    osdcloud-InstallPackageManagement
-    osdcloud-TrustPSGallery
-    osdcloud-InstallPowerShellModule -Name Pester
-    osdcloud-InstallPowerShellModule -Name PSReadLine
-    osdcloud-InstallWinGet
-    if (Get-Command 'WinGet' -ErrorAction SilentlyContinue) {
-        Write-Host -ForegroundColor Green '[+] winget upgrade --all --accept-source-agreements --accept-package-agreements'
-        winget upgrade --all --accept-source-agreements --accept-package-agreements
-    }
-    osdcloud-InstallPwsh
-    Write-Host -ForegroundColor Green "[+] winget.osdcloud.com Complete"
-    $null = Stop-Transcript -ErrorAction Ignore
-}
-#endregion1
+	#Check Winget Install
+	Write-Host "Checking if Winget is installed" -ForegroundColor Yellow
+	$TestWinget = Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -eq "Microsoft.DesktopAppInstaller"}
+	If ([Version]$TestWinGet. Version -gt "2022.506.16.0") 
+	{
+		Write-Host "WinGet is Installed" -ForegroundColor Green
+	}Else 
+		{
+		#Download WinGet MSIXBundle
+		Write-Host "Not installed. Downloading WinGet..." 
+		$WinGetURL = "https://aka.ms/getwinget"
+		$dc.DownloadFile($WinGetURL, "$InstallerFolder\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle")
+		
+		#Install WinGet MSIXBundle 
+		Try 	{
+			Write-Host "Installing MSIXBundle for App Installer..." 
+			Add-AppxProvisionedPackage -Online -PackagePath "$InstallerFolder\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -SkipLicense 
+			Write-Host "Installed MSIXBundle for App Installer" -ForegroundColor Green
+			}
+		Catch {
+			Write-Host "Failed to install MSIXBundle for App Installer..." -ForegroundColor Red
+			} 
+	
+		#Remove WinGet MSIXBundle 
+		#Remove-Item -Path "$InstallerFolder\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -Force -ErrorAction Continue
+		}
